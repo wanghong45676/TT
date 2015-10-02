995719dcbad9ffda8093b805bdf32bb82a12b8a3
wh <wh@whdemacbook-pro.local>
2015-10-02 12:35:03 +0800

    Initial Commit

---
+++ autoCirculateView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autoCirculateView
+//
+//  Created by wh on 15/10/2.
+//  Copyright © 2015年 wh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autoCirculateView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autoCirculateView
+//
+//  Created by wh on 15/10/2.
+//  Copyright © 2015年 wh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
